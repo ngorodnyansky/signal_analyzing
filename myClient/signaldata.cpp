361d474e64a0948212a5f9c6a05dfd3dae00a602
ngorodnyansky <gorodnyansky.n@gmail.com>
2021-09-14 17:58:21 +0300

    Убраны лишние переменные в классе SignalData

--- myClient/signaldata.cpp
+++ myClient/signaldata.cpp
@@ -8,8 +8,8 @@ SignalData::SignalData()
 void SignalData::add(double abscissa, double ordiate){
     y.push_back(ordiate);
     x.push_back(abscissa);
-    if(time*100<=area_limit){
-        xview.push_back(time);
+    if(abscissa*100<=area_limit){
+        xview.push_back(abscissa);
         yview.push_back(ordiate);
     }
     else{
@@ -18,15 +18,15 @@ void SignalData::add(double abscissa, double ordiate){
             yview.swapItemsAt(i,i+1);
         }
         int k=xview.size();
-        xview[k-1]=time;
+        xview[k-1]=abscissa;
         yview[k-1]=ordiate;
     }
     int n = y.size();
     if(n>=3){
         if(((y[n-3]-y[n-2])/h>0 && (y[n-2]-y[n-1])/h<0) || ((y[n-3]-y[n-2])/h<0 && (y[n-2]-y[n-1])/h>0)){
-            extremums_x.push_back(time-0.01);
+            extremums_x.push_back(abscissa-0.01);
             extremums_y.push_back(y[n-2]);
-            extremums_xview.push_back(time-0.01);
+            extremums_xview.push_back(abscissa-0.01);
             extremums_yview.push_back(y[n-2]);
             if(extremums_xview.size()==area_limit){
                 for(int i=0;i<area_limit;++i){
@@ -34,10 +34,9 @@ void SignalData::add(double abscissa, double ordiate){
                     extremums_yview.swapItemsAt(i,i+1);
                 }
                 int j = extremums_xview.size();
-                extremums_xview[j-1]=time-0.01;
+                extremums_xview[j-1]=abscissa-0.01;
                 extremums_yview[j-1]=y[n-2];
             }
         }
     }
-    time+=h;
 }
--- myClient/signaldata.h
+++ myClient/signaldata.h
@@ -5,7 +5,7 @@
 class SignalData
 {
 public:
-    double xBegin=0, xEnd=0, h=0.01, X=0, time=0,amplitude=20,frequency=10;
+    double h=0.01;
     int area_limit=5/h;
     QVector <double> x,y,xview, yview, extremums_x, extremums_y, extremums_xview, extremums_yview;
     void add(double abscissa, double ordiate);
